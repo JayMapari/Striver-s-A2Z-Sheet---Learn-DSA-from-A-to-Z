204b0f763e00693460af28c9aa4144030962e1e1
Jay Mapari <devjay130@gmail.com>
2026-06-15 23:23:54 +0530

    Sum of Beauty of All Substrings

---
+++ Learn the basics/String/Sum_of_Beauty_of_All_Substrings.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int beautySum(string s) {
+    int n = s.size();
+    int ans = 0;
+    for(int i = 0; i < n; i++) {
+        unordered_map<char,int> ump;
+        for(int j = i; j < n; j++) {
+            ump[s[j]]++;
+            int maxfreq = INT_MIN;
+            int minfreq = INT_MAX;
+            for(auto x : ump) {
+                maxfreq = max(maxfreq, x.second);
+                minfreq = min(minfreq, x.second);
+            }
+            ans += (maxfreq - minfreq);
+        }
+    }
+    return ans;
+}
+
+int main(){
+    string str = "aabcb";
+    cout << beautySum(str);
+    return 0;
+}
