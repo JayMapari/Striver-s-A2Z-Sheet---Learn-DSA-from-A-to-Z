3a3fee364c6b97d66150c5f822afd62eef5f967f
Jay Mapari <devjay130@gmail.com>
2026-06-25 23:59:42 +0530

    power set error solved

--- Learn the basics/Recursion/Power_Set.cpp
+++ Learn the basics/Recursion/Power_Set.cpp
@@ -1,29 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
 
 vector<vector<int>> ans;
 
 void solve(int index, vector<int>& arr, vector<int>& nums) {
-    if (index==nums.size()) {
+    if (index == nums.size()) {
         ans.push_back(arr);
         return;
     }
+
     arr.push_back(nums[index]);
-    solve(index+1,arr,nums);
+    solve(index + 1, arr, nums);
+
+
     arr.pop_back();
-    solve(index+1,arr,nums);
+    solve(index + 1, arr, nums);
 }
 
 vector<vector<int>> subsets(vector<int>& nums) {
+    ans.clear();
     vector<int> arr;
-    int index = 0;
-    solve(index,arr,nums);
+    solve(0, arr, nums);
     return ans;
 }
 
-int main(){
-    vector<int>nums = {1,2,3};
-    vector<int>v = subsets(nums);
+int main() {
+    vector<int> nums = {1, 2, 3};
+
+    vector<vector<int>> v = subsets(nums);
+
+    for (auto subset : v) {
+        cout << "{ ";
+        for (int x : subset)
+            cout << x << " ";
+        cout << "}\n";
+    }
+
     return 0;
 }
