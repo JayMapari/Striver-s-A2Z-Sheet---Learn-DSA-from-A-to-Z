2c0178654b43497450041faaed0acfc7a9303d90
Jay Mapari <devjay130@gmail.com>
2026-07-08 23:28:20 +0530

    merge intervals

---
+++ Learn the basics/array_problems/Merge_Intervals.cpp
@@ -0,0 +1,18 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+ 
+ 
+ 
+//  vector<vector<int>> merge(vector<vector<int>>& intervals) {
+//     sort(intervals.begin(),intervals.end());
+//         multimap<int,int>ump;
+//         for(int i =  0; i < intervals.size();i++){
+//             ump[i][0] ,ump[i][1] = intervals[i],intervals[i][1];
+//         }
+//     }
+
+// int main(){
+//     vector<int>v1 = {{1,2},{3,5}};
+//     vector<vector<int>> ans = merge(v1);
+//     return 0;
+// }
