486cbb81cfccc15a780741f796170c8f74e58421
Jay Mapari <devjay130@gmail.com>
2026-08-04 23:51:53 +0530

    Two odd Occuring

---
+++ Learn the basics/bits/Two_odd_Occuring.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoOddNum(vector<int>& nums) {
+    int n = nums.size();
+    int x = 0;
+
+    for (int i = 0; i < n; i++) {
+        x ^= nums[i];
+    }
+
+    int rightmost = (x & (x - 1)) ^ x;
+
+    int XOR1 = 0, XOR2 = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] & rightmost)
+            XOR1 ^= nums[i];
+        else
+            XOR2 ^= nums[i];
+    }
+
+    if (XOR1 > XOR2)
+        return {XOR1, XOR2};
+
+    return {XOR2, XOR1};
+}
+
+int main() {
+    vector<int> arr = {4, 2, 4, 5, 2, 3, 3, 1};
+    vector<int> ans = twoOddNum(arr);
+    cout << ans[0] << " " << ans[1] << endl;
+
+    return 0;
+}
